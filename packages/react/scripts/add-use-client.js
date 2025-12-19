244e9e4b81244f352b9e9a53c7d9cafe535ce513
Adam Mawlawi <adam@mradam.us>
2025-12-19 09:41:59 -0600

    fix: rename add-use-client.js to .cjs for CommonJS compatibility

--- packages/react/scripts/add-use-client.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import fs from 'fs';
-
-const file = 'dist/index.js';
-const content = fs.readFileSync(file, 'utf8');
-fs.writeFileSync(file, '"use client";\n' + content);
-console.log('Added "use client" directive to dist/index.js');
-
