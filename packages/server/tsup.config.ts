24ba60a7cc46f838b2348b4b5e51649c17443f45
Adam Mawlawi <adam@mradam.us>
2025-12-26 15:57:56 -0600

    fix: use shamefully-hoist to flatten node_modules

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -8,34 +8,5 @@ export default defineConfig({
   sourcemap: true,
   splitting: false,
   treeshake: true,
-  // Bundle all dependencies into the output
-  noExternal: [/.*/],
-  // External native modules that can't be bundled
-  external: [
-    '@prisma/client',
-    '@node-rs/argon2',
-    '@simplewebauthn/server',
-    // Node.js built-in modules
-    'crypto',
-    'fs',
-    'path',
-    'os',
-    'stream',
-    'util',
-    'events',
-    'buffer',
-    'http',
-    'https',
-    'net',
-    'tls',
-    'child_process',
-    'worker_threads',
-    'async_hooks',
-    'perf_hooks',
-    'querystring',
-    'url',
-    'assert',
-    'zlib',
-  ],
+  external: ['@prisma/client'],
 });
-
